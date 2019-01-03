e4c6e09e2b11da1509a35ab6d756bfbc6cff1d61
Alex Collier <alexkcollier@gmail.com>
2019-01-03 12:41:39 -0500

    fix: moves base eslint config to its own package

--- index.js
+++ index.js
@@ -4,20 +4,8 @@ module.exports = {
     browser: true,
     node: true
   },
-  extends: ['standard', 'plugin:prettier/recommended', 'prettier', 'plugin:vue/recommended'],
-  plugins: ['standard', 'vue', 'prettier'],
+  extends: ['@acollier/eslint-config'],
   rules: {
-    semi: [2, 'never'],
-    'no-console': 'off',
-    'no-debugger': 'off',
-    'prettier/prettier': [
-      'error',
-      {
-        semi: false,
-        singleQuote: true,
-        printWidth: 100
-      }
-    ],
     'vue/no-v-html': 'off',
     'vue/component-name-in-template-casing': 'off'
   }
