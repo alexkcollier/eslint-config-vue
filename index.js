1ab70608ce82a678c187603150e47c0e2a1ab6b2
Alex Collier <alexkcollier@gmail.com>
2019-06-25 21:29:10 -0400

    feat: updates config

--- index.js
+++ index.js
@@ -2,12 +2,41 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   extends: ['@acollier/eslint-config', 'plugin:vue/recommended'],
   plugins: ['vue'],
   rules: {
-    'vue/no-v-html': 'off',
-    'vue/component-name-in-template-casing': 'off'
-  }
+    'vue/arrow-spacing': ['error', { before: true, after: true }],
+    'vue/block-spacing': ['error', 'always'],
+    'vue/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+    'vue/camelcase': ['warn', { ignoreDestructuring: true }],
+    'vue/comma-dangle': ['error', 'always-multiline'],
+    'vue/component-name-in-template-casing': [
+      'error',
+      'PascalCase',
+      {
+        registeredComponentsOnly: false,
+      },
+    ],
+    'vue/eqeqeq': ['error', 'smart'],
+    'vue/key-spacing': ['error', { beforeColon: false, afterColon: true }],
+    'vue/match-component-file-name': [
+      'error',
+      {
+        extensions: ['vue', 'js', 'jsx'],
+        shouldMatchCase: true,
+      },
+    ],
+    'vue/max-attributes-per-line': [
+      2,
+      {
+        singleline: 1,
+        multiline: {
+          max: 1,
+          allowFirstLine: false,
+        },
+      },
+    ],
+  },
 }
